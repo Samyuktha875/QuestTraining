600b8b9464edef345d157e1741926b63cdae96b8
Samyuktha875 <98074698+Samyuktha875@users.noreply.github.com>
2024-10-07 00:30:04 +0530

    Add files via upload

---
+++ Day3/ReverseArray/reverseArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            var firstHalf = numbers.Take(numbers.Length / 2).Reverse().ToArray();
+            var secondtHalf = numbers.Skip(numbers.Length/2+1).Reverse().ToArray();
+            firstHalf.CopyTo(numbers, 0);
+            secondtHalf.CopyTo(numbers,numbers.Length/2+1 );
+            Console.WriteLine(string.Join(" ", numbers));
+
+
+        }
+    }
+}
